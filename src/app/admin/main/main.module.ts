b397cfe8e21e425d2b7362a96649ce02d40061f8
smartyinkuse1 <olayinkafamutimi@gmail.com>
2020-12-12 20:12:46 +0000

    Admin Scaffold

--- src/app/admin/main/main.module.ts
+++ src/app/admin/main/main.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { UserComponent } from './user/user.component';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent, DashboardComponent, ProductComponent, CategoryComponent, UserComponent, OrderComponent],
   imports: [
     CommonModule,
     MainRoutingModule
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   {path: '', redirectTo: 'customer', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: SignUpComponent},
-  {path: 'customer', loadChildren: ()=> import("../app/customer/customer.module").then(m => m.CustomerModule)}
+  {path: 'customer', loadChildren: ()=> import("../app/customer/customer.module").then(m => m.CustomerModule)},
+  {path: 'admin', loadChildren: ()=> import("../app/admin/admin.module").then(m=> m.AdminModule)}
 
 ];
 
